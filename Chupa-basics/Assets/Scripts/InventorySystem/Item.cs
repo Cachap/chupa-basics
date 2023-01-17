4fcb0139b50fd7ddae7e88f8c106cdc37addc48e
Cachap <maksim.dorofeev.95@mail.ru>
2023-01-18 00:27:45 +0300

    refactoring inventory system

--- Chupa-basics/Assets/Scripts/InventorySystem/Item.cs
+++ Chupa-basics/Assets/Scripts/InventorySystem/Item.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 public class Item : MonoBehaviour
 {
 	[SerializeField] string effect;
+	public Sprite sprite;
+	public readonly static string ItemTag = "Item";
 
 	public string Effect { get { return effect; } }
-
-	public readonly static string ItemTag = "Item";
 }
